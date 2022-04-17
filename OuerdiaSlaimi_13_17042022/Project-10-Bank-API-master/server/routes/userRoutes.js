8c99b0fabe916fc12f0e454ab20dc0bebbf135a4
OuerdiaSlm <ouerdia.slm@gmail.com>
2022-04-17 14:33:28 +0200

    first commit

---
+++ OuerdiaSlaimi_13_17042022/Project-10-Bank-API-master/server/routes/userRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+const userController = require('../controllers/userController')
+const tokenValidation = require('../middleware/tokenValidation')
+
+router.post('/signup', userController.createUser)
+
+router.post('/login', userController.loginUser)
+
+router.post(
+  '/profile',
+  tokenValidation.validateToken,
+  userController.getUserProfile
+)
+
+router.put(
+  '/profile',
+  tokenValidation.validateToken,
+  userController.updateUserProfile
+)
+
+router.get(
+  '/azul',
+  tokenValidation.validateToken,
+  userController.homePage
+)
+
+module.exports = router
